ea2ddffc9af06ac6f98ed5735fc11172efc4b67e
Cameron Shearer <SHE16003894@btc.ac.uk>
2022-05-14 12:03:42 +0100

    Add project files.

---
+++ Cleaning Service/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleaning_Service
+{
+    internal class Customer : User
+    {
+        public char Gender { get; set; }
+        public string? Email { get; set; }
+        public string? Telephone { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? County { get; set; }
+
+        public Customer(string FirstName, string LastName, char Gender, string Email, string Telephone, string Address, string City, string County)
+        {
+            Id = Guid.NewGuid();
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            IsAdmin = false;
+            this.Gender = Gender;
+            this.Email = Email;
+            this.Telephone = Telephone;
+            this.Address = Address;
+            this.City = City;
+            this.County = County;
+        }
+
+        public override string ToString()
+        {
+            return $"Customer Information: {FirstName} {LastName}\n{Gender}\nContact: {Email} + {Telephone}\n{Address}, {City}, {County}";
+        }
+    }
+}
---
+++ Cleaning Service/Enums.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleaning_Service
+{
+    /* Used for the type of property that the service offers */
+
+    public class Enums
+    {
+        public enum ResidentType
+        {
+            DEATCHED,
+            SEMIDEATCHED,
+            TERRACED,
+            BUNGALOW,
+            FLAT,
+            APARTMENT
+        }
+
+        public enum CommercialType
+        {
+            OFFICE,
+            WAREHOUSE,
+            FACTORY,
+            INDUSTRIAL_UNIT,
+            RETAIL
+        }
+    }
+}
